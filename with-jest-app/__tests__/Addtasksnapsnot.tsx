eb1c848011350a9f692c9db583e849f94f64fdff
Shaminder Singh <32118829+shamindermundi@users.noreply.github.com>
2022-03-07 18:19:08 +0530

    Add task snapshot added

--- with-jest-app/__tests__/Addtasksnapsnot.tsx
+++ with-jest-app/__tests__/Addtasksnapsnot.tsx
@@ -1,7 +1,8 @@
 import { render } from "@testing-library/react";
 import AddTask from "@/components/Home/AddTask";
 
-it("renders homepage unchanged", () => {
-    const { container } = render(<AddTask setTaskList={() => console.log("first")} />);
+it("renders add task unchanged", () => {
+    const setTaskList = jest.fn();
+    const { container } = render(<AddTask setTaskList={setTaskList} />);
     expect(container).toMatchSnapshot();
 });
